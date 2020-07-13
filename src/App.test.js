0c9149d9a39db0cfef5d47ae93db1c8592f631fc
Rahul Pote <58665295+TheCodingDarkKnight@users.noreply.github.com>
2020-07-13 20:15:45 +0530

    Add test for App

--- src/App.test.js
+++ src/App.test.js
@@ -2,4 +2,12 @@ import React from "react";
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure } from 'enzyme';
 
+import App from "./App";
+
 configure({ adapter: new Adapter() });
+
+const app = shallow(<App />);
+
+it("Render App correctly", () => {
+    expect(app).toMatchSnapshot();
+})
